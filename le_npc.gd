1d4ef235b1c182530854ce3f69ab6aaf95c9d76a
latvianlazer77 <zacgra62@kingshigh.school.nz>
2025-06-18 15:00:12 +1200

    added ui, worked on main menu

--- le_npc.gd
+++ le_npc.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var e: AnimatedSprite2D = $E
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +14,7 @@ func _unhandled_input(event: InputEvent) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	e.visible = true
--- main_menu.gd
+++ main_menu.gd
@@ -11,8 +11,7 @@ func _ready():
 	
 
 
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_scene.tscn")
+
 	
 
 
@@ -24,3 +23,17 @@ func _on_options_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_scene.tscn")
+
+
+
+	
+
+
+func _on_back_pressed() -> void:
+	main_buttons.visible = true
+	options.visible = false
+	
